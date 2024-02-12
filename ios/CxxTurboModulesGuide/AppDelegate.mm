ed6f7afb77e10b2b38bbbf1770b92f10f08e9cf8
Raul Andrade <raul10.3@hotmail.com>
2024-02-11 21:59:45 -0300

    chore: module registration

--- ios/CxxTurboModulesGuide/AppDelegate.mm
+++ ios/CxxTurboModulesGuide/AppDelegate.mm
@@ -1,6 +1,12 @@
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
+#import <React/CoreModulesPlugins.h>
+#import <ReactCommon/RCTTurboModuleManager.h>
+#import <NativeSampleModule.h>
+
+@interface AppDelegate () <RCTTurboModuleManagerDelegate> {}
+@end
 
 @implementation AppDelegate
 
@@ -26,6 +32,21 @@
 #else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
+}
+
+- (Class)getModuleClassFromName:(const char *)name {
+  return RCTCoreModulesClassProvider(name);
+}
+
+#pragma mark RCTTurboModuleManagerDelegate
+
+- (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:(const std::string &)name
+                                                      jsInvoker:(std::shared_ptr<facebook::react::CallInvoker>)jsInvoker
+{
+  if (name == "NativeSampleModule") {
+    return std::make_shared<facebook::react::NativeSampleModule>(jsInvoker);
+  }
+  return nullptr;
 }
 
 @end
