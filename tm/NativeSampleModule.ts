805ea14e6c7d576c003e069d6f1bf03ecff45f77
Raul Andrade <raul10.3@hotmail.com>
2024-02-11 21:53:54 -0300

    chore: add javascript specification

---
+++ tm/NativeSampleModule.ts
@@ -0,0 +1,8 @@
+import {TurboModule, TurboModuleRegistry} from 'react-native';
+
+export interface Spec extends TurboModule {
+  readonly reverseString: (input: string) => string;
+  readonly toUpperCaseString: (input: string) => string;
+}
+
+export default TurboModuleRegistry.getEnforcing<Spec>('NativeSampleModule');
